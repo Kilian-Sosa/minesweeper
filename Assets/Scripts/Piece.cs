3e912478d6503001284955e184e5629defbce562
Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
2023-11-21 17:29:35 +0000

    refactor: Small name changes

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -6,28 +6,13 @@ public class Piece : MonoBehaviour {
     public int xCoord, yCoord;
     public bool hasBomb;
 
-    public void Start() {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnMouseDown() {
         if (hasBomb) GetComponent<SpriteRenderer>().material.color = Color.red;
         else {
-            // Get the Text component by navigating the hierarchy
             TextMeshProUGUI textComponent = GetComponentInChildren<TextMeshProUGUI>();
-
-            if (textComponent != null) {
-                // The Text component exists, update the text
+            if (textComponent != null) 
                 textComponent.text = Generator.generator.GetNumBombsAround(xCoord, yCoord).ToString();
-            } else {
-                Debug.LogError("Text component not found in the hierarchy of Piece.");
-            }
+            else Debug.LogError("Text component not found in the hierarchy of Piece.");
         }
     }
 }
